d900da2a18b1a30d5bb97a2b53cb02e10aecc82b
santhosh23062004 <kazamakazama143@gmail.com>
2025-06-25 22:30:53 +0530

    day2

---
+++ DataTypes.java
@@ -0,0 +1,39 @@
+package day1;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		
+		String name1="surya";
+		String adjective1="lazy";
+		String noun1="dogs";
+		String noun2="chicken";
+		String adjective2="Stressful";
+		String verb1="dancing";
+		String  noun3="suprise";
+		String  noun4="cats";
+		String adjective3="suprise";
+		String place1="purasivakkam";
+		String noun5="icecream"; 
+		String noun6="glad";
+		int number=2024;
+	
+		
+		
+		
+		
+		
+	
+		String story="this morining"+name1+"wokeup fellings"+adjective1+"it is going to be"+adjective2+"day! "
+			+ "Outside, a bunch of "+noun1+"s were prosting to keep"+noun2+"in storys."
+			+ ". they began to"+verb1+"to the rythem of the"+noun3+"which made all the"+noun4+"very"+adjective3+
+			"Concerned,"+name1+"texted"+noun5+",who flew"+noun1+"to "+place1+"and dropped"+name1+" in a puddle of frozen"+noun5+"."+name1+"woke up in the year"+number+",in the world where"+noun6+
+			"s ruled the world.";
+	 
+	 {
+		 System.out.println(story);
+	 }
+	}
+	
+	
+
+}
