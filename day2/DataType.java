52ab100e09569d4a90b6954fc8307a3a31422a46
santhosh23062004 <kazamakazama143@gmail.com>
2025-06-25 17:06:12 +0530

    day2

---
+++ day2/DataType.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class DataType {
+	
+	public static void main(String[] args) {
+		
+		byte age =127;
+		System.out.println("age "+age);
+		short s=10000;
+		System.out.println("short "+s);
+		int a=10000520;
+		System.out.println("phone "+a);
+		long b=90044545l;
+		System.out.println("long "+b);
+		float f=445.55f;
+		System.out.println("float "+f);
+		double d=45541.55655;
+	  	System.out.println("double "+d);
+		boolean t=true;
+		System.out.println("it is correct "+t);
+		char surya='S';
+		System.out.println("charcter "+surya);
+		System.out.println("add" +(56+45));
+		
+		
+	}
+
+}
